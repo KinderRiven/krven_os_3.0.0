32ffae87092af29e9138b1936981afcc5b4d3c19
K <1339764596@qq.com>
2017-09-15 12:57:26 +0800

    update

--- tools/build.c
+++ tools/build.c
@@ -10,26 +10,18 @@
 #define STDIN_FD        0
 #define STDOUT_FD       1
 #define SECTOR_SIZE     512
-#define BOOT_SIZE       (SECTOR * 1)
-#define SETUP_SIZE      (SECTOR * 4)
+#define BOOT_SIZE       (SECTOR_SIZE * 1)
+#define SETUP_SIZE      (SECTOR_SIZE * 4)
 
 void write_boot(char *boot) {
     //write boot
     int fd, len;
-<<<<<<< HEAD
     char buf[BOOT_SIZE];
     assert((fd = open(boot, O_RDONLY)) > 0);
-=======
-    char buf[BUFFER_SIZE];
-    //check
-    assert(argc > 1);
-    assert((fd = open(argv[1], O_RDONLY, 0)) > 0);
->>>>>>> 4776a2ec70addf5c56724ce89df49ab17e27d63f
     len = read(fd, buf, sizeof(buf));
     //one boot sector must be 512B
     assert(len == BOOT_SIZE);
     //write boot sector to floppy img
-<<<<<<< HEAD
     len = write(STDOUT_FD, buf, BOOT_SIZE);
     assert(len == BOOT_SIZE);
     close(fd);
@@ -52,14 +44,8 @@ void write_setup(char *setup) {
 int main(int argc, char **argv) {
     //check
     assert(argc > 1);
-    write_boot(argc[1]);
-    write_setup(argc[2]);
+    write_boot(argv[1]);
+    write_setup(argv[2]);
     return 0;
-=======
-    len = write(STDOUT_FD, buf, SECTOR_SIZE);
-    assert(len == SECTOR_SIZE);
-	close(fd);
-	return 0;
->>>>>>> 4776a2ec70addf5c56724ce89df49ab17e27d63f
 }
 
