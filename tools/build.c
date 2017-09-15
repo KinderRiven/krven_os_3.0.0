4aab69a8c70e7dff263603ce72c3af73d9bc2d9c
K <1339764596@qq.com>
2017-09-15 12:53:40 +0800

    update

--- tools/build.c
+++ tools/build.c
@@ -7,23 +7,45 @@
 #include <fcntl.h>
 #include <assert.h>
 
-#define STDIN_FD  0
-#define STDOUT_FD 1
-#define BUFFER_SIZE 1024
-#define SECTOR_SIZE 512
+#define STDIN_FD        0
+#define STDOUT_FD       1
+#define SECTOR_SIZE     512
+#define BOOT_SIZE       (SECTOR * 1)
+#define SETUP_SIZE      (SECTOR * 4)
 
-int main(int argc, char **argv) {
+void write_boot(char *boot) {
+    //write boot
     int fd, len;
-    char buf[BUFFER_SIZE];
-    //check
-    assert(argc > 1);
-    assert((fd = open(argv[1], O_RDONLY)) > 0);
+    char buf[BOOT_SIZE];
+    assert((fd = open(boot, O_RDONLY)) > 0);
     len = read(fd, buf, sizeof(buf));
     //one boot sector must be 512B
-    assert(len == SECTOR_SIZE);
+    assert(len == BOOT_SIZE);
     //write boot sector to floppy img
-    len = write(STDOUT_FD, buf, SECTOR_SIZE);
-    assert(len == SECTOR_SIZE);
+    len = write(STDOUT_FD, buf, BOOT_SIZE);
+    assert(len == BOOT_SIZE);
+    close(fd);
+}
+
+void write_setup(char *setup) {
+    //write setup
+    int fd, len;
+    char buf[SETUP_SIZE];
+    assert((fd = open(setup, O_RDONLY)) > 0);
+    len = read(fd, buf, sizeof(buf));
+    //one setup sector must be 2048B
+    assert(len == SETUP_SIZE);
+    //write setup sector to floppy img
+    len = write(STDOUT_FD, buf, SETUP_SIZE);
+    assert(len == SETUP_SIZE);
+    close(fd);
+}
+
+int main(int argc, char **argv) {
+    //check
+    assert(argc > 1);
+    write_boot(argc[1]);
+    write_setup(argc[2]);
     return 0;
 }
 
