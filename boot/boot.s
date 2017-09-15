95f6bfb2e4d423b8a00d4610ac5d2d0c59f4b8eb
kinderriven <1339764596@qq.com>
2017-09-15 16:51:42 +0800

    update

--- boot/boot.s
+++ boot/boot.s
@@ -6,28 +6,41 @@ boot_start:
 	mov	ds,	ax
 	mov	es,	ax
 	mov	ss,	ax
-	jmp	$
-move_init:
+
+;move boot from [07c00h] -> [90000h]
+move_boot_init:
 	xor	bx,	bx
 	mov	ax,	boot_addr
 	mov	ds,	ax
 	mov	ax,	init_addr
 	mov	es,	ax
 	mov	cx,	256
-	
-do_move:
+
+move_boot:
 	mov	ax, [ds:bx]
 	mov	[es:bx], ax
 	add	bx,	2
-	loop do_move
-	jmp init_addr:move_9000h	
+	loop move_boot
+	jmp init_addr:jmp_90000h	
 
-move_9000h:
+;jmp to [90000h]
+jmp_90000h:
 	mov	ax,	cs
 	mov	ds,	ax
 	mov es,	ax
-	mov	ss,	ax	
-	jmp	$
+	mov	ss,	ax
+
+;prepare to load setup sector
+load_setup_init:
+
+;start load
+load_setup:				
+
+
+;jmp to setup
+jmp_setup:
+	jmp	$	
+	
 
 boot_fill:
     times 510 - ($ - $$) db 0
--- boot/setup.s
+++ boot/setup.s
@@ -1,2 +1,10 @@
+setup_start:
+	mov	ax,	0xffff
+	mov	bx,	0xffff
+	mov	cx,	0xffff	
+	jmp	$
+	xor	di,	di
+	xor	si,	si
 
-times 2048 - ($ - $$) db 0x11
+setup_fill:
+	times 2048 - ($ - $$) db 0x11
